d09ed9b134401fcc24805c5c4c4f2e37476532d4
dipakdusane <dipakdusane@users.noreply.github.com>
2022-05-01 09:37:52 +0530

    LoadBalancer for prod-web

--- sample-prod-env/prod.tf
+++ sample-prod-env/prod.tf
@@ -10,6 +10,22 @@ resource "aws_s3_bucket" "prod_tf_course" {
   
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az_a" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Terraform_managed": "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az_b" {
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Terraform_managed": "true"
+  }
+}
+
 resource "aws_security_group" "prod_web"{
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound" 
@@ -40,12 +56,12 @@ resource "aws_security_group" "prod_web"{
 
 resource "aws_instance" "prod_web"{
   count = 2
-
+  
   ami           = "ami-065fb54436c0e2d57"
   instance_type = "t2.nano"
-
+ 
   vpc_security_group_ids = [
-    aws_security_group.prod_web.id
+    aws_security_group.prod_web.id 
   ]
 
   tags = {
@@ -57,8 +73,22 @@ resource "aws_eip" "prod_web" {
   tags = {
     "Terraform_managed": "true"
   }
-
+  
   instance = aws_instance.prod_web.0.id
-
 }
+
+resource "aws_elb" "prod_web_lb" {
+  name = "prod-web-lb"
+  
+  instances        = aws_instance.prod_web.*.id 
+  subnets         = [aws_default_subnet.default_az_a.id, aws_default_subnet.default_az_b.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80 
+    instance_protocol = "http"
+    lb_port           = 80 
+    lb_protocol       = "http"
+  }
+} 
 
